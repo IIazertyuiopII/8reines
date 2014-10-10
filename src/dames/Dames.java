bd6643d711c46b474519469a854f23c9ff896f6f
IAZERTYUIOPI <IAZERTYUIOPI@130.66.205.16>
2014-10-10 16:45:20 +0200

    Hey cuties 42

---
+++ src/dames/Dames.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dames;
+
+/**
+ *
+ * @author IAZERTYUIOPI
+ */
+public class Dames {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           
+        // TODO code application logic here
+        PlateauJeu p = new PlateauJeu(42, true);
+        p.Go();
+    }
+    
+}
---
+++ src/dames/PlateauJeu.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dames;
+
+
+/**
+ *
+ * @author IAZERTYUIOPI
+ */
+public class PlateauJeu {
+    
+    private int[] tableau;
+    
+    public PlateauJeu(int taille, boolean isRandom)
+    {
+        tableau = new int[taille];
+
+        for (int i = 0;i<taille;i++){
+              tableau[i] = i;
+        }
+        
+    }
+    
+    public int[] getTableau(){
+      return tableau;  
+    }
+    
+    public void permuter(int[] t,int i, int j)
+    {
+       int swap = t[i];
+       t[i] = t[j];
+       t[j] = swap;
+       
+    }
+    
+    public int evaluatePositionIfPermutait(int i, int j)
+    {
+       int[] tmpTab = tableau.clone();
+       permuter(tmpTab, i, j);
+       return evaluatePosition(tmpTab);
+    }
+    
+    public String toString() {
+    
+        String output = "";
+        for (int i = 0;i<tableau.length;i++){
+            for(int j = 0;j<tableau.length;j++) {
+                output+= tableau[i]==j? "D":".";
+                output+=" ";
+            } 
+            output+="\n";
+        }
+        output+="\n";
+        output+="Position score : "+evaluatePosition(tableau)+"\n";
+        return output;
+    }
+    
+    public int evaluatePosition(int[] t) {
+        
+        int score=0;
+        
+        for (int i = 0;i<t.length;i++){
+            for (int j = 0;j<t.length;j++){
+                if( Math.abs(t[i]-t[j])==Math.abs(i-j) && i!=j)
+                {
+                    score-=10;
+                }
+            }
+        }
+        return score;
+    }
+
+    public void Go() {
+        
+        System.out.print(toString());
+        int compteur = 0;
+        
+        while (evaluatePosition(tableau)!=0)
+        {
+            choisirLaMeilleurePermutation();
+            compteur++;
+            System.out.println(toString());
+        }
+
+        System.out.println(toString());
+        System.out.println("Nombre de permutations :" + compteur);
+    }
+    
+    public void choisirLaMeilleurePermutation() {
+
+    int indiceLigneMeilleurePermutation = 0;
+    int indiceColonneMeilleurePermutation = 0;  
+    int scoreMeilleurePermutation = -100000;
+    int scorePermutationCourante;
+        
+    for(int i = 0; i<tableau.length;i++){
+        for(int j = 0; j<tableau.length;j++){
+            scorePermutationCourante = evaluatePositionIfPermutait(i, j);
+            if(i!=j && scorePermutationCourante > scoreMeilleurePermutation) {
+                indiceLigneMeilleurePermutation = i;
+                indiceColonneMeilleurePermutation = j;
+                scoreMeilleurePermutation = scorePermutationCourante;
+            }
+            
+        }   
+    }
+      
+        permuter(tableau,indiceLigneMeilleurePermutation,indiceColonneMeilleurePermutation);
+    
+    }
+}
